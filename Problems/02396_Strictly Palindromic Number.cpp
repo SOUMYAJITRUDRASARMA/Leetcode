274201aed90658ec76897cbfc71f399f083d72a9
SOUMYAJITRUDRASARMA <soumyajitrudrasarma@gmail.com>
2022-09-07 22:10:49 +0530

    Create 02396_Strictly Palindromic Number.cpp

---
+++ Problems/02396_Strictly Palindromic Number.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        // representation of n in base 'n - 2' is 12 which is akways non-palindromic
+        return false;
+    }
+};
