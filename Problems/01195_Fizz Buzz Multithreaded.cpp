ad5f9faead44bc010617675e13549a8b470c0d8a
SOUMYAJITRUDRASARMA <soumyajitrudrasarma@gmail.com>
2022-11-16 23:24:55 +0530

    srs

--- Problems/01195_Fizz Buzz Multithreaded.cpp
+++ Problems/01195_Fizz Buzz Multithreaded.cpp
@@ -1,4 +1,88 @@
+#include <semaphore.h>
+
 class FizzBuzz {
+private:
+    int n;
+    int i;
+    sem_t sem_fizz, sem_buzz, sem_fizzbuzz, sem_number;
+    bool exit = false;
+
+public:
+    FizzBuzz(int n) {
+        this->n = n;
+        i = 0;
+        
+        sem_init(&sem_fizz, 1, 0);
+        sem_init(&sem_buzz, 1, 0);
+        sem_init(&sem_fizzbuzz, 1, 0);
+        sem_init(&sem_number, 1, 0);
+
+        callNext();
+    }
+
+    void callNext()
+    {
+        if(++i > n)
+        {
+            exit = true;
+            sem_post(&sem_fizzbuzz), sem_post(&sem_fizz), sem_post(&sem_buzz), sem_post(&sem_number);
+
+            return;
+        }
+
+        if(!(i % 3) && !(i % 5)) sem_post(&sem_fizzbuzz);
+        else if(!(i % 3) && (i % 5)) sem_post(&sem_fizz);
+        else if((i % 3) && !(i % 5)) sem_post(&sem_buzz);
+        else sem_post(&sem_number);
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        while(true)
+        {
+            sem_wait(&sem_fizz);
+            if(exit) return;
+            printFizz();
+            callNext();
+        }
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        while(true)
+        {
+            sem_wait(&sem_fizz);
+            if(exit) return;
+            printBuzz();
+            callNext();
+        }
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        while(true)
+        {
+            sem_wait(&sem_fizz);
+            if(exit) return;
+            printFizzBuzz();
+            callNext();
+        }
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+        while(true)
+        {
+            sem_wait(&sem_fizz);
+            if(exit) return;
+            printNumber(i);
+            callNext();
+        }
+    }
+};
+
+
+/* class FizzBuzz {
 private:
     int n, i;
     mutex m;
@@ -93,4 +177,4 @@ public:
             c.notify_all();
         }
     }
-};
+}; */
